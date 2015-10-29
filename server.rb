07e241fe2a5a2a401c0d490abca924762c2f997f
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-10-28 18:43:22 -0700

    server now does what it is supposed to do

--- server.rb
+++ server.rb
@@ -1,16 +1,18 @@
 require_relative 'lib/config/httpd_config'
 require_relative 'lib/config/mime_types'
+require_relative 'lib/worker'
 
 require 'socket'
 
 module WebServer
   class Server
 
-    attr_reader :httpd_settings, :mime_settings
+    attr_reader :httpd_settings, :mime_settings, :logger
 
     def initialize(options = {})
       @httpd_settings = Config::HttpdConfig.new
       @mime_settings  = Config::MimeTypes.new
+      @logger         = Logger.new(@httpd_settings, )
     end
 
     def start
@@ -21,7 +23,7 @@ module WebServer
 
         Thread.abort_on_exception = true
         Thread.start(server.accept) do |client_connection|
-          puts 'connection received'
+          Worker.new(client_connection, self).process_request
         end
       end
     end
